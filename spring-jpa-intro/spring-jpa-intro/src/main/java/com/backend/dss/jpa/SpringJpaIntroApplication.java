112cf80e5aa5522b043020377dc4e106406f4406
dscifo <diego.scifo@globallogic.com>
2024-07-29 19:01:52 -0300

    New project jpa-intro. Start jpa course.

---
+++ spring-jpa-intro/spring-jpa-intro/src/main/java/com/backend/dss/jpa/SpringJpaIntroApplication.java
@@ -0,0 +1,13 @@
+package com.backend.dss.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaIntroApplication.class, args);
+    }
+
+}
---
+++ spring-jpa-intro/spring-jpa-intro/src/test/java/com/backend/dss/jpa/SpringJpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.dss.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
